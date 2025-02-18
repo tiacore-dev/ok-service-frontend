6f5760447cadc2df5eded7dcf9aab07c825a73f7
Ilia Timofeev <ilya48007@gmail.com>
2025-02-18 14:39:43 +0700

    push

--- src/pages/auth/component/account.tsx
+++ src/pages/auth/component/account.tsx
@@ -1,17 +1,12 @@
 import { Button, Descriptions, Space } from "antd";
 import * as React from "react";
 import { useDispatch } from "react-redux";
-import {
-  IauthToken,
-  useAuthData,
-  // checkPermission,
-} from "../../../hooks/useAuth";
+import { useAuthData } from "../../../hooks/useAuth";
 import { authlogout } from "../../../store/modules/auth";
 import { useloadSourse } from "../../../components/App/App";
-
+import { useApi } from "../../../hooks/useApi";
 
 interface notificationsSubscribeData {
-  authToken: IauthToken;
   endpoint: string;
   p256dh: string;
   auth: string;
@@ -23,31 +18,42 @@ interface notificationsSubscribeResponse {
 export const Account = () => {
   const data = useAuthData();
   const dispatch = useDispatch();
+  const { apiPost } = useApi();
 
-  // const handleSubscribe = React.useCallback(async () => {
-  //   try {
-  //     const registration = await navigator.serviceWorker.ready;
-  //     const subscription = await registration.pushManager.subscribe({
-  //       userVisibleOnly: true,
-  //       applicationServerKey: process.env.REACT_APP_WP_SVS,
-  //     });
+  const handleSubscribe = React.useCallback(async () => {
+    // navigator.serviceWorker.ready.then((reg) => {
+    //   reg.pushManager.getSubscription().then((subscription) => {
+    //     const options = subscription.options;
+    //     console.log(options.applicationServerKey); // the public key
+    //   });
+    // });
 
-  //     const subscriptionData = JSON.parse(JSON.stringify(subscription));
+    try {
+      const registration = await navigator.serviceWorker.ready;
+      console.log(registration);
 
-  //     useApi<notificationsSubscribeResponse, notificationsSubscribeData>(
-  //       "notifications",
-  //       "subscribe",
-  //       {
-  //         authToken: token,
-  //         endpoint: subscriptionData.endpoint,
-  //         p256dh: subscriptionData.keys.p256dh,
-  //         auth: subscriptionData.keys.auth,
-  //       }
-  //     );
-  //   } catch (error) {
-  //     console.error("Error subscribing to Push:", error);
-  //   }
-  // }, []);
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: process.env.REACT_APP_WP,
+      });
+      console.log(subscription);
+
+      const subscriptionData = JSON.parse(JSON.stringify(subscription));
+      console.log(subscriptionData);
+
+      apiPost<notificationsSubscribeResponse, notificationsSubscribeData>(
+        "subscriptions",
+        "subscribe",
+        {
+          endpoint: subscriptionData.endpoint,
+          p256dh: subscriptionData.keys.p256dh,
+          auth: subscriptionData.keys.auth,
+        }
+      );
+    } catch (error) {
+      console.error("Error subscribing to Push:", error);
+    }
+  }, []);
 
   const { load, clearStates } = useloadSourse();
   const refresh = () => {
@@ -66,16 +72,13 @@ export const Account = () => {
         </Descriptions.Item>
       </Descriptions>
       <Space direction="vertical">
-        <Button onClick={refresh}>
-          Обновить данные
-        </Button>
-        {/* <Button size="large" onClick={handleSubscribe}>
+        <Button onClick={refresh}>Обновить данные</Button>
+        <Button size="large" onClick={handleSubscribe}>
           Подписаться
-        </Button> */}
+        </Button>
 
         <Button
           onClick={() => {
-
             dispatch(authlogout());
           }}
         >
