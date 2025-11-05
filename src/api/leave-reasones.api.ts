54d6f219faf78012ba342b20e66be6712129782d
Ilya Timofeev <ilya48007@gmail.com>
2025-11-06 00:53:14 +0700

    feat: leaves (ok) #991

---
+++ src/api/leave-reasones.api.ts
@@ -0,0 +1,10 @@
+// import { apiClient } from "./base";
+// import type { ILeaveReason } from "../interfaces/leaveReasones/ILeaveReason";
+
+// export const fetchLeaveReasones = async (): Promise<ILeaveReason[]> => {
+//   const { data } = await apiClient.get<{ reasons: ILeaveReason[] }>(
+//     "/leaves/reasones/all",
+//   );
+
+//   return data.reasons;
+// };
